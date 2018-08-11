ca57864be201151a77ee0448e8d65a6fefd21cce
Ivan Ganev <iganevru@gmail.com>
2018-08-11 03:09:27 +0300

    ini

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,14 @@
 <template>
+<div>
   <v-carousel>
     <v-carousel-item v-for="(src, i) in images" :src="src" :key="i"/>
   </v-carousel>
+  <v-container>
+    <p>
+      Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+    </p>
+  </v-container>
+  </div>
 </template>
 
 <script>
