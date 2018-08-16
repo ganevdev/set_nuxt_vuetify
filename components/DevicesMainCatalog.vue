22eaa38bb7460b1a066e9e8053c65043074d0162
Ivan Ganev <iganevru@gmail.com>
2018-08-16 07:26:57 +0300

    заменил "" на null - так лучше работает

--- components/DevicesMainCatalog.vue
+++ components/DevicesMainCatalog.vue
@@ -13,6 +13,7 @@
     <!-- Каталог МЫШЕК -->
     <!-- <div v-if="this.$route.name ==='devices-mouse' "> -->
 
+    <!-- радио фильтры -->
     <v-btn-toggle v-model="mouse_form">
       <v-btn flat value="ergonomic">
         ergonomic
@@ -23,6 +24,20 @@
     </v-btn-toggle>
     <p>{{mouse_form}}</p>
     <p>&nbsp;</p>
+
+    <v-btn-toggle v-model="mouse_sensor">
+      <v-btn flat value="optical">
+        optical
+      </v-btn>
+      <v-btn flat value="laser">
+        laser
+      </v-btn>
+    </v-btn-toggle>
+    <p>{{mouse_sensor}}</p>
+    <p>&nbsp;</p>
+
+    <!-- сортировка -->
+    <!-- TODO - сделать компонент для сортировки что бы не повторять в каждом разделе девайсов -->
 
     <!-- <transition-group name="fade" tag="section"> -->
     <div v-for="device in FilteredDevices" :key="device.ASIN">
@@ -48,8 +63,9 @@ export default {
   data: function() {
     return {
       devicesJSON: devicesJSON,
-      text_search: "",
-      mouse_form: ""
+      text_search: null,
+      mouse_form: null,
+      mouse_sensor: null
     };
   },
   components: {
@@ -59,28 +75,36 @@ export default {
   computed: {
     FilteredDevices() {
       return this.devicesJSON.filter(device => {
-        let matches = true;
+        let filter_matches = true;
 
         // из https://codepen.io/andreasremdt/pen/rvQJYR?editors=1010
         // freeCodeCamp Twitch Viewer
 
-        // matches определяет для каждого девайса по отдельности
+        // filter_matches определяет для каждого девайса по отдельности
         // пройдет ли он в лист FilteredDevices или нет
         // каждый следующий if это фильтр,
-        // если девайс его не проходит то matches = false
+        // если девайс его не проходит то filter_matches = false
 
+        // фильтр по поиску
         if (
-          this.text_search !== "" &&
+          this.text_search !== null &&
           !device.title.toLowerCase().includes(this.text_search.toLowerCase())
         ) {
-          matches = false;
+          filter_matches = false;
         }
 
-        if (this.mouse_form !== "" && this.mouse_form !== device.mouse_form) {
-          matches = false;
+        // фильтр по мышиным радио кнопкам
+        if (this.mouse_form !== null && this.mouse_form !== device.mouse_form) {
+          filter_matches = false;
+        }
+        if (
+          this.mouse_sensor !== null &&
+          this.mouse_sensor !== device.mouse_sensor
+        ) {
+          filter_matches = false;
         }
 
-        if (matches) return device;
+        if (filter_matches) return device;
       });
     }
   }
