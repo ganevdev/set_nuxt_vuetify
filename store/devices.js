513d7a4bc212e5c487af3ba3716ec2412c0e7b4b
Ivan Ganev <iganevru@gmail.com>
2018-08-30 17:53:30 +0300

    ini

--- store/devices.js
+++ store/devices.js
@@ -1,5 +1,7 @@
 import devicesJSON from "~/assets/devices.json";
 
+// по статье https://medium.com/a-man-with-no-server/static-site-generators-nuxt-js-2fa9782d27c8
+
 export const state = () => ({
   devices: devicesJSON
 });
